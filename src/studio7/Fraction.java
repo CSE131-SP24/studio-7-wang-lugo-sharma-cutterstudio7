2d5660b28d3a87254cca6e5e09d80895e0681678
ash3 <ash3@localhost>
2024-03-28 12:48:42 -0500

    fraction

---
+++ src/studio7/Fraction.java
@@ -0,0 +1,33 @@
+package studio7;
+
+public class Fraction {
+	private int num;
+	private int den;
+	
+	
+	public Fraction(int num, int den) {
+		this.num = num;
+		this.den = den;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fraction f1 = new Fraction(1,2);
+		Fraction f2 = new Fraction(3,4);
+		Fraction f3 = new Fraction(3,9);
+		f1.multiple(f2);
+	}
+	
+	public void sum() {
+	
+	}
+	
+	public void multiple(Fraction f1) {
+		mult = f1.num * this.num 
+				
+	}
+	public void reciprocal() {
+		
+	}
+
+}
